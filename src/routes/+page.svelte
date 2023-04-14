0409f79a8c23ef9f458d7e5521a7133366c9aa59
Gabriel Delight <gabrieldelight08@gmail.com>
2023-04-14 17:20:42 +0100

    Created tecmplates

---
+++ src/routes/+page.svelte
@@ -0,0 +1,91 @@
+
+<script>
+	import  Myheader from "./MyHeader.svelte";
+</script>
+<Myheader />
+<div class="video_container">
+	<div class="video_wrapper">
+
+	</div>
+
+	<div class="video_wrapper">
+
+	</div>
+</div>
+
+<br>
+<div class="footer_container">
+		<button>Join</button>
+		<button>Leave</button>
+		<button>Invite</button>
+
+</div>
+
+<style>
+	.video_container {
+		width: 80%;
+		height: 60vh;
+		margin:  auto;	
+		display: flex;
+		padding: 10px;
+		justify-content: space-between;
+		
+	}
+
+	@media (max-width: 600px) {
+		.video_container  {
+			flex-direction: column;
+			width: 100%;
+		}
+	}
+
+	.video_wrapper  {
+		flex-basis: 45%;
+		background-color: rgb(43 47 49);
+		border: 10px;
+		height: 100%;
+		border-radius: 10px;
+	}
+
+	
+
+	.footer_container  {
+		width: 50%;
+		margin: auto;
+		background-color: black;
+		padding: 10px;
+		box-shadow: 8px 9px 10px #d9d9d9;
+		border-radius: 10px;
+	}
+
+	@media (max-width: 600px) {
+		.footer_container  {
+			width: 90%;
+			margin: auto;
+
+		}
+}
+
+	.footer_container button {
+		padding: 10px 20px;
+		margin-right: 10px;
+		cursor: pointer;
+		color: white;
+		border-radius: 5px;
+		border: none;
+		cursor: pointer;
+	}
+	
+	.footer_container button:nth-child(1)  {
+		background-color: green;
+	}
+
+
+	.footer_container button:nth-child(2)  {
+		background-color: red;
+	}
+
+	.footer_container button:nth-child(3)  {
+		background-color: gray;
+	}
+</style>
---
+++ src/routes/MyHeader.svelte
@@ -0,0 +1,55 @@
+<header>
+<div class="form">
+    <input placeholder="Dolby Secret Token" type="text">
+    <button>Authenticate</button>
+</div>
+
+<div class="status_section">
+    <button>Inactive</button>
+    <div class="round"></div>
+</div>
+</header>
+
+<style>
+    header {
+        padding: 20px;
+        background-color: black;
+        margin-bottom: 3rem;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    header input {
+        width: 300px;
+        padding: 10px;
+
+    }
+
+
+    .form button {
+        padding: 10px;
+        margin-left: 10px;
+        border-radius: 5px;
+        background-color: hotpink;
+        border: none;
+        outline: none;
+        color: white;
+        cursor: pointer;
+        outline: none;
+    }
+
+    .round {
+        width: 10px;
+        height: 10px;
+        background-color: red;
+        border-radius: 10px;
+        margin-left: 10px;
+
+    }
+
+    .status_section {
+        display: flex;
+        align-items: center;
+    }
+</style>
